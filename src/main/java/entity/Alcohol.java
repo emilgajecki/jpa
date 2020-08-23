9ee7aae7a5f020b9665e0824ce3ac6c198b307c3
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-23 12:15:45 +0200

    zmiana na metody

--- src/main/java/entity/Alcohol.java
+++ src/main/java/entity/Alcohol.java
@@ -1,12 +1,16 @@
 package entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
 public class Alcohol {
     @Id
+    // powoduje generowanie klucza przez autoincrement
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
